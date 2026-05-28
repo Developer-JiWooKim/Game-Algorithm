14a51f9d6a2a209629ac4b046bdfa60363b14bbb
JiWoo Kim <wldnkim00@gmail.com>
2026-05-28 12:08:00 +0900

    Update PracticeTransform.cs

--- Assets/Scripts/PracticeTransform.cs
+++ Assets/Scripts/PracticeTransform.cs
@@ -4,12 +4,7 @@ using UnityEngine.InputSystem;
 public class PracticeTransform : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
