7c5401af8900be9efac2ffda11f2615bf3419f15
JiWoo Kim <wldnkim00@gmail.com>
2026-05-28 12:57:26 +0900

    Matrix Study

---
+++ Assets/Scripts/MatrixTest.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MatrixTest : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Matrix4x4 worldMatrix = transform.localToWorldMatrix;
+
+        Vector3 localPos = new Vector3(2f, 0, 0);
+
+        Vector3 worldPos = worldMatrix.MultiplyPoint3x4(localPos);
+
+        Gizmos.color = Color.yellow;
+
+        Gizmos.DrawLine(transform.position, worldPos);
+        Gizmos.DrawSphere(worldPos, .1f);
+    }
+}
